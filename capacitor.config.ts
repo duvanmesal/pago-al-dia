355967a3a2423063ea95a0758424e9186066b048
duvan_mesa <Duvanmesa2415@gmail.com>
2026-07-13 18:35:09 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pago-al-dia',
+  webDir: 'www'
+};
+
+export default config;
